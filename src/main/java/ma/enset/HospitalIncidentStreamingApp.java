7dc264f5e5ef9e7762c3814dc9d11bac416794d2
Wahiba275 <ouahibabouzyan@gmail.com>
2023-11-30 14:15:02 -0800

    second commit

--- src/main/java/ma/enset/HospitalIncidentStreamingApp.java
+++ src/main/java/ma/enset/HospitalIncidentStreamingApp.java
@@ -7,7 +7,9 @@ import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.Trigger;
+
 import static org.apache.spark.sql.functions.*;
+
 
 public class HospitalIncidentStreamingApp {
     public static void main(String[] args) throws Exception {
